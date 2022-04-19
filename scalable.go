6ec65b72b7d8f3b9a76c3114f6c3b66089b9e7b1
Louis Thibault <l.thibault@sentimens.com>
2022-04-19 18:26:41 -0400

    Update copyright notice.

--- scalable.go
+++ scalable.go
@@ -1,4 +1,5 @@
 // Copyright (c) 2014 Dataence, LLC. All rights reserved.
+// Copyright (c) 2020 Blocknative Corporation. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use sbf file except in compliance with the License.
